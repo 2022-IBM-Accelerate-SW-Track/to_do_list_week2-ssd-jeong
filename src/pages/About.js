009a570327cacd3bd455cd13b27500baa3df6df2
Daniel Jeong <sdjeong0404@gmail.com>
2022-06-20 22:34:10 -0400

    final commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import profile_pic from "../assets/profile_pic.jpeg";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
         <div class="split left">
           <div className="centered">
             <img
@@ -18,10 +17,9 @@ export default class About extends Component {
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Daniel Jeong</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+              Hobbies: Squash, Stocks, and Violin
             </div>
           </div>
         </div>
